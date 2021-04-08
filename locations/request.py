4d42d5de2f4dd102ee5d228e1b0f31b51c12210e
Ccourter11 <courtercalvin@yahoo.com>
2021-04-08 11:58:39 -0500

    ch 2&3

---
+++ locations/request.py
@@ -0,0 +1,35 @@
+LOCATIONS = [
+    {
+      "name": "Nashville North West",
+      "id": 1,
+      "address": "8422 Johnson Pike"
+    },
+    {
+      "id": 2,
+      "name": "Nashville South",
+      "address": "209 Emory Drive"
+    },
+    {
+      "name": "Jimmy Johns1",
+      "id": 3,
+      "address": "Denver Colorado"
+    }
+  ]
+
+def get_all_locations():
+    return LOCATIONS
+
+    # Function with a single parameter
+def get_single_location(id):
+    # Variable to hold the found location, if it exists
+    requested_location = None
+
+    # Iterate the LOCATION list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for location in LOCATIONS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if location["id"] == id:
+            requested_location = location
+
+    return requested_location
